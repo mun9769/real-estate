840663f7e44a1eb1936ec8e0ce834329fdcde0c9
mun9769 <mun9769@naver.com>
2021-01-21 19:45:21 +0900

    to mysql

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from pyvirtualdisplay import Display
+import pymysql
 
 display = Display(visible=1, size=(1920, 1080))
 display.start()
@@ -20,20 +21,43 @@ driver.find_element_by_xpath('//*[@id="idJiwonNm1"]/option[8]').click()
 driver.find_element_by_xpath("//*[@id=\"main_btn\"]/a/img").click()
 
 
-items = []
 sum = 0
 i = 1
 s = 2
+
+conn = pymysql.connect(host="localhost",user = 'root',password=None,db='test',charset='utf8')
+curs = conn.cursor()
+
+sql = """insert into real_estate(no,사건번호,물건번호,소재지,비고,감정평가액,담당계,link)
+                            value(%s,%s,%s,%s,%s,%s,%s,%s)"""
+
+number = 1
 while True:
     item0 = driver.find_elements_by_class_name("Ltbl_list_lvl0")
     item1 = driver.find_elements_by_class_name("Ltbl_list_lvl1")
-    sum += len(item0 + item1)
-    print(sum)
+    items = item0 + item1
+
+    for item in items:
+        curs.execute(sql,(str(number),
+                        item.find_elements_by_css_selector("td")[1].text,
+                        item.find_elements_by_css_selector("td")[2].text,
+                        "none",
+                        "none",
+                        item.find_elements_by_css_selector("td")[5].text,
+                        item.find_elements_by_css_selector("td")[6].text,
+                        "none"))
+    
+        number += 1
+        # print(item.find_elements_by_css_selector("td")[1].text)
+
+    # sum += len(item0 + item1)
+    # print(sum)
 
     page = driver.find_elements_by_class_name("page2")
     children = page[0].find_elements_by_tag_name("a")
-    # print(len(children))
-    # print(children[0].find_element_by_tag_name("span").text)
+
+
+
     try:
         if i == 1:
             children[0].click()
@@ -48,6 +72,8 @@ while True:
     if s > 11 :
         s = 2
 
+conn.commit()
+conn.close()
 
 
 for i in range(5):
